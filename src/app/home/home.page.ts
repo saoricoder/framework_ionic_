6b7e5cda5c4ccd5a529c62713a0dc52336109562
saoricoder <blrodriguez3@espe.edu.ec>
2024-07-27 12:39:16 -0500

    Cambios RAaaa!
    xD

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] //2ejemplo
 })
 export class HomePage {
+
   constructor() {}
+
+  handleButtonClick(buttonName: string) {
+    console.log(`${buttonName} clicked!`);
+    // Puedes realizar otras acciones aquí.
+  }
+
+  handleCardClick(cardName: string) {
+    console.log(`${cardName} clicked!`);
+    // Puedes agregar lógica adicional aquí.
+  }
+
+  handleItemClick(itemName: string) {
+    console.log(`${itemName} clicked!`);
+    // Puedes agregar lógica adicional aquí.
+  }
 }
+
