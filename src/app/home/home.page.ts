2d26d8d9d6b9a68a69708ad763ca04387a792646
saoricoder <blrodriguez3@espe.edu.ec>
2024-07-27 13:04:09 -0500

    Version viaje en tarjetas
    hhay 3 opciones para usar ripple effect

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
@@ -13,20 +12,26 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLab
 export class HomePage {
 
   constructor() {}
-
+//btn personalizazo
   handleButtonClick(buttonName: string) {
     console.log(`${buttonName} clicked!`);
     // Puedes realizar otras acciones aquí.
   }
-
+/*tarjetas
   handleCardClick(cardName: string) {
     console.log(`${cardName} clicked!`);
     // Puedes agregar lógica adicional aquí.
-  }
+  }*/
 
+  handleCardClick(destination: string) {
+    console.log(`Información sobre: ${destination}`);
+    // Aquí puedes redirigir a una página de detalles del destino o mostrar un mensaje.
+  }
+//Lista
   handleItemClick(itemName: string) {
     console.log(`${itemName} clicked!`);
     // Puedes agregar lógica adicional aquí.
   }
+
 }
 
